8492a44fe07780f7b7aa50fb515428603b20286c
usagiarchiving <usagiarchiving@gmail.com>
2026-07-29 08:16:46 +0900

    Update render.js

--- editor/github/render.js
+++ editor/github/render.js
@@ -566,7 +566,7 @@ function updateOutput(skipPreviewUpdate = false) {
 
                 let bubMarginTop = '15px';
                 if (isSameAsPrev) {
-                    bubMarginTop = '3px'; 
+                    bubMarginTop = '1px'; 
                 } else if (isPrevDiag) {
                     bubMarginTop = mt_10; 
                 } else {
